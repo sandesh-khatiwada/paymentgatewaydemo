16ebd1686c22e045b29afb115dd9ea8d5ad3265a
Sandesh Khatiwada <khatiwadasandesh01@gmail.com>
2025-06-08 10:58:21 +0545

    Complete migration from spring data jpa to spring data jdbc

--- src/main/java/com/sandesh/paymentgatewaydemo/entity/PaymentRequest.java
+++ src/main/java/com/sandesh/paymentgatewaydemo/entity/PaymentRequest.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
@@ -15,23 +16,29 @@ import org.springframework.data.relational.core.mapping.Table;
 public class PaymentRequest {
 
     @Id
+    @Column("payment_request_id")
     private Long id;
 
+    @Column("app_id")
     private AppId appId;
 
     private Double amount;
 
     private String particular;
 
+    @Column("ref_id")
     private String refId;
 
     private String remarks;
 
+    @Column("debit_status")
     private Status debitStatus;
 
+    @Column("credit_status")
     private Status creditStatus;
 
     private Status status;
 
+    @Column("user_id")
     private Long userId;
 }
